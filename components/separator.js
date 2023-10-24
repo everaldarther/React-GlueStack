49bd41fb08b64f675ce7e8a9d30c2ddabbab5223
everaldarther <everaldarther21@gmail.com>
2023-10-24 13:55:51 +0700

    Initial commit

---
+++ components/separator.js
@@ -0,0 +1,7 @@
+import { View } from "@gluestack-ui/themed";
+// Functional Component with props
+const Separator = (props) => {
+  return <View style={{ height: props.height }}></View>;
+};
+
+export default Separator;
